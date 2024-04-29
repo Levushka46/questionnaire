6f0914bcdc423a6f98513264d230d8d270abed41
Lev Sergeev <levushka46@mail.ru>
2024-04-29 17:30:13 +0300

    Remove test views

--- answers/urls.py
+++ answers/urls.py
@@ -5,7 +5,6 @@ from . import views
 
 urlpatterns = [
     path("", views.SignInView.as_view(), name="index"),
-    path("page_dev/", views.page_dev, name="page_dev"),
     path("page/<int:page_id>/", views.PageView.as_view(), name="page"),
     path("done/", views.DoneView.as_view(), name="done"),
     path("no_surveys/", views.NoSurveysView.as_view(), name="no_surveys"),
--- answers/views.py
+++ answers/views.py
@@ -10,25 +10,6 @@ from answers.widgets import RadioSelect, CheckboxSelectMultiple
 from formpages.models import Page, Survey
 
 
-def page_dev(request):
-    class TestForm(PageForm):
-        question_1 = forms.CharField(label="Question 1?")
-        question_radio = forms.ChoiceField(
-            choices=[(1, "Option 1"), (2, "Option 2")],
-            widget=RadioSelect,
-        )
-        question_checkboxes = forms.MultipleChoiceField(
-            choices=[(1, "Option 1"), (2, "Option 2")],
-            widget=CheckboxSelectMultiple,
-        )
-    form = TestForm(request.POST or None)
-    page = {
-        "title": "Page title",
-        "description": "Page description",
-    }
-    return render(request, "answers/page.html", {"page": page, "form": form})
-
-
 def page(request, page_id):
     page = get_object_or_404(Page, id=page_id)
     form = PageForm(page, request.POST or None, user=request.user)
@@ -39,10 +20,6 @@ def page(request, page_id):
             pass
 
     return render(request, "answers/page.html", {"page": page, "form": form})
-
-
-def sign_in(request):
-    return render(request, "answers/sign_in.html")
 
 
 class PageView(LoginRequiredMixin, View):
